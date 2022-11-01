35671c7885815e11472f9cbeaf68e9f97933ad09
Boris <boris@mbp-de-boris.lan>
2022-11-01 14:32:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data.json";
+// import logoNetflix from "./image/LogoNetflix.png";
 
 function App() {
+  // const tab1 = data;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        {/* <img className="logo" src={logoNetflix} alt="Netflix" /> */}
+        Netflix
+      </div>
+      <div className="container">{console.log(data)}</div>
     </div>
   );
 }
